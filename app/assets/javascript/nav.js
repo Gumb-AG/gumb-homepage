d3dee0c82c2898012430ad78e14865c78777c396
Jonathan Arbely <dev@jonathanarbely.de>
2020-03-02 15:14:55 +0100

    Enhanced fixed nav

--- app/assets/javascript/nav.js
+++ app/assets/javascript/nav.js
@@ -9,15 +9,13 @@ jQuery(document).ready(function () {
 });
 
 function fixedNav() {
-    if (jQuery(window).scrollTop() >= 775) {
+    if (jQuery(window).scrollTop() >= 1275) {
         jQuery('.nav').addClass('nav--fixed');
         jQuery('.nav__brand').hide();
-        //jQuery('.nav__brand--wordmark').show();
         jQuery('nav .btn').addClass('btn--inverted');
     } else {
         jQuery('.nav').removeClass('nav--fixed');
         jQuery('.nav__brand').show();
-        //jQuery('.nav__brand--wordmark').hide();
         jQuery('nav .btn').removeClass('btn--inverted');
     }
 }
