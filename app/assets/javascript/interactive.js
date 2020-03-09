016b6765df6ca0fc03f564867c4a6100c3423fd5
Jonathan Arbely <dev@jonathanarbely.de>
2020-03-10 00:27:29 +0100

    About page WIP

--- app/assets/javascript/interactive.js
+++ app/assets/javascript/interactive.js
@@ -1,1 +1,15 @@
+$(function() {
+    // Our values UI logic
+    $('.value').click(function(){
+        // Define which value was clicked
+        var v = $(this).data('value');
 
+        // Set active h3
+        $('.value').removeClass('gradient');
+        $('#value' +v).addClass('gradient');
+
+        // Set active Text
+        $('.valueText').hide();
+        $('#value' +v +'Text').show();
+    });
+});
