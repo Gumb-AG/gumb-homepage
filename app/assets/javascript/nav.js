2d6a9b57532182ef52046428f4f4bb9f1a04fa6d
Jonathan Arbely <dev@jonathanarbely.de>
2020-03-25 20:29:05 +0100

    Sticky nav

--- app/assets/javascript/nav.js
+++ app/assets/javascript/nav.js
@@ -1,28 +1,40 @@
-/*jQuery(document).ready(function () {
-    
-    // Scrollmenu
-    fixedNav();
-    jQuery(window).scroll(function () {
-        fixedNav();
-    });
-
-});
-
-function fixedNav() {
-    if (jQuery(window).scrollTop() >= 1275) {
-        jQuery('.nav').addClass('nav--fixed');
-        jQuery('.nav__brand').hide();
-        jQuery('nav .btn').addClass('btn--inverted');
-    } else {
-        jQuery('.nav').removeClass('nav--fixed');
-        jQuery('.nav__brand').show();
-        jQuery('nav .btn').removeClass('btn--inverted');
-    }
-}*/
-
-// Mobile Nav
 $(function() {
+    // Mobile Nav
     $('#mobileNav').change(function() {
         window.location.replace( $("#mobileNav option:selected").val() );
     });
+
+    // Fix nev when scrolling (or swiping) up
+    var lastScrollTop = 0;
+    $('body').scroll(function() {
+        var st = $(this).scrollTop();
+        if (st > lastScrollTop){
+            //console.log('scroll down');
+
+            hideFixedNav();
+        } else {
+            //console.log('scroll up');
+
+            if ($('body').scrollTop() > 900)
+                showFixedNav();
+            else if ($('body').scrollTop() < 820)
+                hideFixedNav();
+        }
+        lastScrollTop = st;
+    });
 });
+
+function showFixedNav() {
+    // Slide-in animation and fix Nav
+    $('.nav').addClass('nav--fixed').parent().addClass('nav__wrapper--fixed').removeClass('nav__wrapper--fixed--remove');
+}
+
+function hideFixedNav() {
+    // Slide-up animation
+    $('.nav__wrapper').addClass('nav__wrapper--fixed--remove');
+
+    // Remove fixed Nav state after 500ms (after Slide-up animation has finished)
+    setTimeout(function(){ 
+        $('.nav').removeClass('nav--fixed').parent().removeClass('nav__wrapper--fixed');
+     }, 300);
+}
